75637927ac2df766a3a932d372a7952e764861f8
Pedro Perez <pperez@intersysconsulting.com>
2022-01-23 00:06:03 -0700

    create pipeline

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkPipelineStack extends Stack {
@@ -12,5 +13,13 @@ export class CdkPipelineStack extends Stack {
     // const queue = new sqs.Queue(this, 'CdkPipelineQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const repo = 'PedroJPerez/cdk-pipeline';
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'My Pipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub(repo, 'master'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+    })
+  });
   }
 }
